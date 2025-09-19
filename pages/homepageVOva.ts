2fe1766772708410686f8ebb1bb5de722f368705
vovan-u-s <oversoul6661@gmail.com>
2025-09-18 20:51:43 -0500

    +

--- pages/homepageVOva.ts
+++ pages/homepageVOva.ts
@@ -2,16 +2,16 @@ import { expect, Locator, Page } from '@playwright/test'
 export class HomePage {
 
     titleMessage: Locator;
-    totalBalance: Locator;
+    totalTitle: Locator;
     constructor(page: Page) {
         this.titleMessage = page.locator('div[class="logo-label"]')
-        this.totalBalance = page.locator('div[class="balance hidden-mobile"]')
+        this.totalTitle = page.locator('div[class="element-wrapper compact pt-4"] h6[class="element-header"]')
     }
     async expectedMessage(ourMessage: string): Promise<void> {
         await expect(this.titleMessage).toHaveText(ourMessage)
     }
     async expectingBalance(ourBAlance:string):Promise<void>{
-        await expect(this.totalBalance).toHaveText(ourBAlance)
+        await expect(this.totalTitle).toHaveText(ourBAlance)
     }
 }
 
